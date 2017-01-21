b812520c6f4db673aed7697ec6042677110aee62
Alex Schearer <aschearer@gmail.com>
2017-01-20 22:30:52 -0800

    Adding lifespan to missile and removing it when through

--- RobotFrog/Assets/Scripts/Missile.cs
+++ RobotFrog/Assets/Scripts/Missile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +10,9 @@ public class Missile : MonoBehaviour {
     [SerializeField]
     private float movementpeed;
 
+    [SerializeField]
+    private float lifespan;
+
     private Vector3 movementSpeedInternal;
 
     // Use this for initialization
@@ -18,10 +22,18 @@ public class Missile : MonoBehaviour {
             -this.movementpeed * Mathf.Sin(heading.y * Mathf.Deg2Rad), 
             0,
             -this.movementpeed * Mathf.Cos(heading.y * Mathf.Deg2Rad));
+
+        this.StartCoroutine(this.CommitSuicide());
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         this.transform.localPosition += this.movementSpeedInternal * Time.deltaTime;
-	}
+    }
+
+    private IEnumerator CommitSuicide()
+    {
+        yield return new WaitForSeconds(this.lifespan);
+        GameObject.Destroy(this.gameObject);
+    }
 }
