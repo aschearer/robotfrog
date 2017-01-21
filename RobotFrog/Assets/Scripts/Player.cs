cea11ba06b8b7b43eaa045e640a4e3e0d0ec0d59
Alex Schearer <aschearer@gmail.com>
2017-01-20 21:15:40 -0800

    Testing for input on player update. Adding script to prefab

--- RobotFrog/Assets/Scripts/Player.cs
+++ RobotFrog/Assets/Scripts/Player.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
-
-	// Use this for initialization
+    
 	void Start () {
-		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float fight = Input.GetAxis("Fire1");
+
+        Debug.Log(string.Format("H: {0}. V: {1}. F: {2}", horizontal, vertical, fight));
+    }
 }
