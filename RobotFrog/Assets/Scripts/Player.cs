28f9612ac912eda33ddd6ed65fb8ad9bec30d33e
Alex Schearer <aschearer@gmail.com>
2017-01-20 21:43:17 -0800

    Adding missile prefab, script. Pressing fire adds missile to scene

--- RobotFrog/Assets/Scripts/Player.cs
+++ RobotFrog/Assets/Scripts/Player.cs
@@ -8,18 +8,29 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private Vector3 verticalMovementSpeed;
 
+    [SerializeField]
+    public Missile MissilePrefab;
+
     void Start () {
 	}
 	
 	void Update () {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        float fight = Input.GetAxis("Fire1");
+        bool fire = Input.GetButtonDown("Fire1");
 
         Vector3 movementVector = Vector3.zero;
         movementVector += this.horizontalMovementSpeed * horizontal;
         movementVector += this.verticalMovementSpeed * vertical;
 
         this.transform.localPosition += movementVector;
+        if (fire)
+        {
+            var missile = GameObject.Instantiate(this.MissilePrefab.gameObject);
+            missile.transform.SetParent(this.transform.parent);
+            missile.transform.localPosition = this.transform.localPosition;
+            var missileView = missile.GetComponent<Missile>();
+            missileView.Owner = this;
+        }
     }
 }
